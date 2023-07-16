744e5f4e60056f3af7bd7fb07c68df5a9dc59b00
Yi-Hsiu Lee <tmrmds.yhl@gmail.com>
2023-07-16 11:59:23 +0800

    feat: add user authentication in todo and home routers

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,17 +3,25 @@ const router = express.Router()
 const User = require('../../models/user')
 const passport = require('passport')
 
+// login
 router.get('/login', (req, res) => {
     res.render('login')
 })
 
+// login
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/users/login'
+}))
+
+// register
 router.get('/register', (req, res) => {
     res.render('register')
 })
 
-router.post('/login', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/users/login'
-}))
+router.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/users/login')
+})
 
 module.exports = router
