a62cc6bbd6d3aa05ad10373c45a1aee379becfe6
Yi-Hsiu Lee <tmrmds.yhl@gmail.com>
2023-07-16 11:39:26 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
+const usePassport = require('./config/passport') // 載入設定檔，要寫在 express-session 以後
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -22,6 +23,8 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }))
+
+usePassport(app) // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
